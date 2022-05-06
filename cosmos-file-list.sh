f861048451059011820f189179968945cb5cdd22
Seb-figment <sebastien.nobert@figment.io>
2022-05-06 17:05:04 -0400

    generate src

--- cosmos-file-list.sh
+++ cosmos-file-list.sh
@@ -1,15 +1,11 @@
 # /bin/bash
 
-ls -1 $(grep -slR 'message Msg' ./cosmos-sdk/proto ./ibc-go/proto | egrep '\.proto$') \
+ls -1 $(grep -slR 'message Msg' ./cosmos-sdk/proto | egrep '\.proto$') \
     $(egrep -slR 'enum|message' ./cosmos-sdk/third_party/proto/tendermint/ | egrep '\.proto$') \
     $(egrep -slR 'enum|message' ./cosmos-sdk/third_party/proto/google/ | egrep '\.proto$') \
     ./cosmos-sdk/proto/cosmos/bank/v1beta1/bank.proto \
     ./cosmos-sdk/proto/cosmos/base/v1beta1/coin.proto \
     ./cosmos-sdk/proto/cosmos/gov/v1beta1/gov.proto \
     ./cosmos-sdk/proto/cosmos/staking/v1beta1/staking.proto \
-    ./cosmos-sdk/proto/ibc/core/client/v1/client.proto \
-    ./cosmos-sdk/proto/ibc/core/channel/v1/channel.proto \
-    ./cosmos-sdk/proto/ibc/core/connection/v1/connection.proto \
-    ./cosmos-sdk/proto/ibc/core/commitment/v1/commitment.proto \
     ./cosmos-sdk/third_party/proto/confio/proofs.proto \
     | sort | uniq
