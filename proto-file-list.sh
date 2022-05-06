86c29b50963c22fdf7fe9247e8ba829616081169
Seb-figment <sebastien.nobert@figment.io>
2022-05-06 16:32:00 -0400

    add ibc-go submodule

--- proto-file-list.sh
+++ proto-file-list.sh
@@ -1,16 +1,15 @@
 # /bin/bash
 
-ls -1 $(grep -slR 'message Msg' ./third_party | egrep '\.proto$') \
-    $(egrep -slR 'enum|message' ./third_party/tendermint/ | egrep '\.proto$') \
-    $(egrep -slR 'enum|message' ./third_party/google/ | egrep '\.proto$') \
-    ./third_party/cosmos/authz/v1beta1/authz.proto \
-    ./third_party/cosmos/bank/v1beta1/bank.proto \
-    ./third_party/cosmos/base/v1beta1/coin.proto \
-    ./third_party/cosmos/gov/v1beta1/gov.proto \
-    ./third_party/cosmos/staking/v1beta1/staking.proto \
-    ./third_party/ibc/core/client/v1/client.proto \
-    ./third_party/ibc/core/channel/v1/channel.proto \
-    ./third_party/ibc/core/connection/v1/connection.proto \
-    ./third_party/ibc/core/commitment/v1/commitment.proto \
-    ./third_party/confio/proofs.proto \
+ls -1 $(grep -slR 'message Msg' ./cosmos-sdk/proto ./ibc-go/proto | egrep '\.proto$') \
+    $(egrep -slR 'enum|message' ./cosmos-sdk/third_party/proto/tendermint/ | egrep '\.proto$') \
+    $(egrep -slR 'enum|message' ./cosmos-sdk/third_party/proto/google/ | egrep '\.proto$') \
+    ./cosmos-sdk/proto/cosmos/bank/v1beta1/bank.proto \
+    ./cosmos-sdk/proto/cosmos/base/v1beta1/coin.proto \
+    ./cosmos-sdk/proto/cosmos/gov/v1beta1/gov.proto \
+    ./cosmos-sdk/proto/cosmos/staking/v1beta1/staking.proto \
+    ./cosmos-sdk/proto/ibc/core/client/v1/client.proto \
+    ./cosmos-sdk/proto/ibc/core/channel/v1/channel.proto \
+    ./cosmos-sdk/proto/ibc/core/connection/v1/connection.proto \
+    ./cosmos-sdk/proto/ibc/core/commitment/v1/commitment.proto \
+    ./cosmos-sdk/third_party/proto/confio/proofs.proto \
     | sort | uniq
