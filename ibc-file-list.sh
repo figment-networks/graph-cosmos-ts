19d30c3d95fa6bcc67c056c6941fb89a6f08115f
Seb-figment <sebastien.nobert@figment.io>
2022-05-12 13:42:45 -0400

    merge build + fix comment

--- ibc-file-list.sh
+++
@@ -1,8 +0,0 @@
-# /bin/bash
-
-ls -1 $(grep -slR 'message Msg' ./ibc-go/proto | egrep '\.proto$') \
-    ./ibc-go/proto/ibc/core/client/v1/client.proto \
-    ./ibc-go/proto/ibc/core/channel/v1/channel.proto \
-    ./ibc-go/proto/ibc/core/connection/v1/connection.proto \
-    ./ibc-go/proto/ibc/core/commitment/v1/commitment.proto \
-    | sort | uniq
